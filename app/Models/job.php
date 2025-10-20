d43214a323ee8adaafb5ba0c88956f41613a68ae
FalzexProgrammer <muhammadfirdausbinmohdazamli@gmail.com>
2025-10-21 03:20:59 +0800

    pivot

--- app/Models/job.php
+++ app/Models/job.php
@@ -3,7 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 
 
 class Job extends Model
@@ -11,4 +12,14 @@ class Job extends Model
     use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+    
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'job_tag', 'job_listing_id', 'tag_id');
+    }
 }
