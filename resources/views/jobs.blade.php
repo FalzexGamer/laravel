d4429e60ce4ae5732252d91e5d019a29670695c9
FalzexProgrammer <muhammadfirdausbinmohdazamli@gmail.com>
2025-10-22 22:10:04 +0800

    day 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,9 +1,13 @@
 <x-layout>
     <x-slot:heading>job listing</x-slot:heading>
-    @foreach ($jobs as $job)
-        <a href="jobs/{{ $job['id'] }}">
-            <h2 class="text-xl font-bold">{{ $job['title'] }}</h2>
-            <p class="mb-4 text-gray-600">{{ $job['salary'] }}</p>
-        </a>
-    @endforeach
+
+    <div class="space-y-4">
+        @foreach ($jobs as $job)
+            <a href="jobs/{{ $job['id'] }}"
+                class="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h2 class="text-xl font-bold">{{ $job->employer->name }}</h2>
+                <p class="mb-4 text-gray-600">{{ $job['title'] }} - RM {{ $job['salary'] }}</p>
+            </a>
+        @endforeach
+    </div>
 </x-layout>
